3f577aef86160b9c26b817c3fc3ede12500b69b0
apondon <apondon@126.com>
2020-10-09 11:24:03 +0800

    192代码

---
+++ index.js
@@ -0,0 +1,14 @@
+// alert('hello!!!')
+
+// 这是一个单行注释
+
+/* 这是多行注释 */ 
+
+// alert('单行注释1')
+// alert('单行注释2')
+
+/*alert('多行注释1')
+alert('多行注释2')*/
+
+document.write('你好')
+document.write('<div>我是div</div>')
